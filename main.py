c0623cf7854671d5bcce169f668e5de4d8a15624
Kyle on Ubuntu <kyle.primary@gmail.com>
2016-10-13 12:58:44 -0700

    file input completed

--- main.py
+++ main.py
@@ -2,14 +2,20 @@
 # File: main.py
 # Date Created: 10/11/16
 # Description: Algorithms Project 1, 
+# Usage: python main.py file.txt 
+#  or use ./run after initiating run
 
 import sys # for getting input file
 
-def main():
-	filename = sys.argv[-1]
-	openedFile = open(filename)
+def OPT():
+	return
 
-	peopleList = list(openedFile)
+def main():
+	filename = sys.argv[-1] # The file name given
+	openedFile = open(filename) # the file is opened
+
+	peopleList = list(openedFile.read()) # Get a list of all the types of people
+	peopleList.pop() # pop off the '\n'
 	print(peopleList)
 	return
 main()
